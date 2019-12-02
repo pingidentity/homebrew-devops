e28330a45872a432f9426cd1692dbfc53484d7ef
Terry Sigle <terrysigle@pingidentity.com>
2019-12-02 07:34:09 +0000

    Creating release v0.4.2

--- Formula/ping-devops.rb
+++ Formula/ping-devops.rb
@@ -1,14 +1,15 @@
 class PingDevops < Formula
   desc "PingIdentity DevOps Command-Line Tool"
   homepage "https://devops.pingidentity.com"
-  url "https://github.com/pingidentity/ping-devops/archive/v0.4.tar.gz"
-  sha256 "2b6a678ebebba6f746d4aa85e1a289ae3f4e8c44ff4821210daa6e750cf8d5d7"
+  url "https://github.com/pingidentity/ping-devops/archive/v0.4.2.tar.gz"
+  sha256 "8ee8ea4f98bb398208de9775d366e7b3cddf2d08f00471d9db3d62ddedbcaf24"
 
   depends_on "kubernetes-cli"
   depends_on "kubectx"
   depends_on "kustomize"
   depends_on "gettext"
   depends_on "stern"
+  depends_on "jq"
 
   def install
     bin.install "ping-devops"
