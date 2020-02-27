b21166ba4c59c9763667413bfb0865dd42c21cd4
tsigle <terrysigle@pingidentity.com>
2020-02-27 11:14:55 -0600

    Updating brew installer to v0.4.5

--- Formula/ping-devops.rb
+++ Formula/ping-devops.rb
@@ -1,8 +1,8 @@
 class PingDevops < Formula
   desc "PingIdentity DevOps Command-Line Tool"
   homepage "https://devops.pingidentity.com"
-  url "https://github.com/pingidentity/ping-devops/archive/v0.4.4.tar.gz"
-  sha256 "eb05d35a257e30190535ea9003bf574b645306f3f4e5d5870322e96f1a427991"
+  url "https://github.com/pingidentity/ping-devops/archive/v0.4.5.tar.gz"
+  sha256 "003b6fd66f652a6c999b6b96d9963d1360fa7c668fb377394577932955c9c102"
 
   depends_on "kubernetes-cli"
   depends_on "kubectx"
