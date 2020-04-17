41fddaa4a446eb03360de1814e5aca172ada9577
Terry Sigle <terry@sigle.com>
2020-04-17 10:34:25 -0500

    updating to version 0.5.2

--- Formula/ping-devops.rb
+++ Formula/ping-devops.rb
@@ -1,8 +1,8 @@
 class PingDevops < Formula
   desc "PingIdentity DevOps Command-Line Tool"
   homepage "https://devops.pingidentity.com"
-  url "https://github.com/pingidentity/ping-devops/archive/v0.5.1.tar.gz"
-  sha256 "ba5a0642483d3e3777c99fff0a77902aec6f98d397975618dfd180dea34ae3ab"
+  url "https://github.com/pingidentity/ping-devops/archive/v0.5.2.tar.gz"
+  sha256 "6264d65a445fe3c3f01ffd537f2210f071f61bfcb56e8c06ead2800615e66ec7"
 
   depends_on "kubernetes-cli"
   depends_on "kubectx"
