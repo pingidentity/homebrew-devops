daca0c1cbb843aad94e5d5151bbdbb6dbd25171c
Terry Sigle <terry@sigle.com>
2020-04-19 10:21:26 -0500

    GDO-188 and GDO-199

--- Formula/ping-devops.rb
+++ Formula/ping-devops.rb
@@ -1,8 +1,8 @@
 class PingDevops < Formula
   desc "PingIdentity DevOps Command-Line Tool"
   homepage "https://devops.pingidentity.com"
-  url "https://github.com/pingidentity/ping-devops/archive/v0.5.2.tar.gz"
-  sha256 "6264d65a445fe3c3f01ffd537f2210f071f61bfcb56e8c06ead2800615e66ec7"
+  url "https://github.com/pingidentity/ping-devops/archive/v0.5.3.tar.gz"
+  sha256 "9e017bf1108d550d8f65941fc26f344a564c40a2b8ac5b78b70d542617f2b815"
 
   depends_on "kubernetes-cli"
   depends_on "kubectx"
