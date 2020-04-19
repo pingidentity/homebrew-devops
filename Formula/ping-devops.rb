8aed46ef19540c84a8ec524355742186d2aed5e9
Terry Sigle <terry@sigle.com>
2020-04-19 15:23:32 -0500

    Adding replace of bash_profile

--- Formula/ping-devops.rb
+++ Formula/ping-devops.rb
@@ -17,6 +17,26 @@ class PingDevops < Formula
 
   end
 
+  # move previous version of etc/bash_profile.ping-devops
+  def move_profile
+    old_profile = etc/"bash_profile.ping-devops"
+    new_profile = etc/"bash_profile.ping-devops.default"
+    # Else, we're already using the latest version:
+    return unless new_profile.exist?
+
+    backup = etc/"bash_profile.ping-devops.old"
+    opoo "Existing bash_profile.ping-devops will be moved to #{backup}."
+    mv old_profile, backup
+    mv new_profile, old_profile
+  end
+
+  def post_install
+    move_profile
+    ohai "Verify your existing Ping Identity DevOps confis with"
+    ohai "  ping-devops info"
+    ohai "  ping-devops config"
+  end
+
   test do
     assert_match "Usage:", shell_output("#{bin}/ping-devops -h 2>&1")
   end
